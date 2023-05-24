037fa85d914f7bd3e182efd4c7cb7b5beb2a8530
saramnim <12hyeji24@gmail.com>
2023-05-24 15:41:26 +0900

    fix: service, app.js

--- app.js
+++ app.js
@@ -5,29 +5,46 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const corsConfig = require('./config/corsConfig.json');
 
+const indexRouter = require('./routes/index');
+
+const models = require('./models/index');
 const logger = require('./lib/logger');
 
 dotenv.config();
-const app = express();
 const { NODE_ENV, PORT, LOGGER_LEVEL } = process.env;
+
+const app = express();
 logger.info('app start');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser);
-app.use(logger('dev'));
+// DB 연결 확인 및 table 생성
+models.sequelize.authenticate().then(() => {
+  logger.info('DB connection success');
+
+  // sequelize sync (table 생성)
+  models.sequelize.sync().then(() => {
+    logger.info('Sequelize sync success');
+  }).catch((err) => {
+    logger.error('Sequelize sync error', err);
+  });
+}).catch((err) => {
+  logger.error('DB Connection fail', err);
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
